7f2a5d6e65200d2ccf6900cb3e1e60b0e801d74d
Kiran Karandikar <38068652+Kiran-Karandikar@users.noreply.github.com>
2023-04-19 15:16:48 -0400

    build(Global): WIP: CSS changes and Chat Room.
    Took 29 minutes

--- base/chat/urls.py
+++ base/chat/urls.py
@@ -1,6 +1,6 @@
 """URL mappings for the user API."""
 # Django Libraries
-from django.urls import path
+from django.urls import path, re_path
 
 # Project Libraries
 from chat.views import all_rooms, room_detail, token
@@ -9,7 +9,7 @@ from chat.views import all_rooms, room_detail, token
 app_name = "chat"
 
 urlpatterns = [
-    path(r"^$", all_rooms, name="all_rooms"),
-    path(r"token$", token, name="token"),
-    path(r"rooms/(?P<slug>[-\w]+)/$", room_detail, name="room_detail"),
+    path("", all_rooms, name="all_rooms"),
+    path("token", token, name="token"),
+    re_path(r"rooms/(?P<slug>[-\w]+)/$", room_detail, name="room_detail"),
 ]
