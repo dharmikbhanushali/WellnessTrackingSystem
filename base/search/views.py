f085ba702c7a3667edc44c7b5cdb50732d2aa4f3
Kiran Karandikar <38068652+Kiran-Karandikar@users.noreply.github.com>
2023-04-23 17:58:11 -0400

    feature(Global): Added Basic Search Functionality.
    Took 1 hour 2 minutes

--- base/search/views.py
+++ base/search/views.py
@@ -1,20 +1,29 @@
 # Django Libraries
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
-from django.views.generic import DetailView, ListView
+from django.db.models import Q
+from django.shortcuts import render
+from django.views import View
 
 # Project Libraries
 from core.models import Workouts
 
 
-class SearchTrainersView(LoginRequiredMixin, DetailView, ListView):
-    paginate_by = 100  # if pagination is desired
-    model = Workouts
-
-    def get(self, request, *args, **kwargs):
-        for key, value in request.GET.items():
-            print(key, value)
-        for key, value in kwargs.items():
-            print(key, value)
-
-        return HttpResponse("<h1>Hello HttpResponse</h1>")
+class SearchTrainersView(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        search_term = request.POST.get("textfield", None)
+        result = Workouts.objects.none()
+        if search_term:
+            search_term = search_term.strip().lower()
+            qs = Workouts.objects.all()
+            result = qs.filter(
+                Q(title__icontains=search_term)
+                | Q(description__icontains=search_term)
+                | Q(trainer__username__icontains=search_term)
+                | Q(trainer__first_name__icontains=search_term)
+                | Q(trainer__last_name__icontains=search_term)
+                | Q(trainer__email__icontains=search_term)
+            )
+        else:
+            messages.error(request, "Empty Search Field Value.")
+        return render(request, "search/index.html", {"queryset": result})
